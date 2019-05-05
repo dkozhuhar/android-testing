e35e433ddc737a9e087797822b3cd20186e3458e
Dmitry Kozhukhar <dna.coj@gmail.com>
2019-05-06 00:23:26 +0300

    7.2

--- app/src/androidTest/java/com/example/android/testing/notes/notes/NotesScreenTest.java
+++ app/src/androidTest/java/com/example/android/testing/notes/notes/NotesScreenTest.java
@@ -25,6 +25,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -97,38 +98,41 @@ public class NotesScreenTest {
 
     @Test
     public void clickAddNoteButton_opensAddNoteUi() throws Exception {
-        fail("Implement step 7");
-//        // Click on the add note button
-//        onView(withId(R.id.fab_add_notes)).perform(click());
-//
-//        // Check if the add note screen is displayed
-//        onView(withId(R.id.add_note_title)).check(matches(isDisplayed()));
+        //fail("Implement step 7");
+        // Click on the add note button
+        onView(withId(R.id.fab_add_notes)).perform(click());
+
+        // Check if the add note screen is displayed
+        onView(withId(R.id.add_note_title)).check(matches(isDisplayed()));
     }
 
     @Test
     public void addNoteToNotesList() throws Exception {
-        fail("Implement step 7");
-//        String newNoteTitle = "Espresso";
-//        String newNoteDescription = "UI testing for Android";
-//
-//        // Click on the add note button
-//        onView(withId(R.id.fab_add_notes)).perform(click());
-//
-//        // Add note title and description
-//        // Type new note title
-//        onView(withId(R.id.add_note_title)).perform(typeText(newNoteTitle), closeSoftKeyboard());
-//        onView(withId(R.id.add_note_description)).perform(typeText(newNoteDescription),
-//                closeSoftKeyboard()); // Type new note description and close the keyboard
-//
-//        // Save the note
-//        onView(withId(R.id.fab_add_notes)).perform(click());
-//
-//        // Scroll notes list to added note, by finding its description
-//        onView(withId(R.id.notes_list)).perform(
-//                scrollTo(hasDescendant(withText(newNoteDescription))));
-//
-//        // Verify note is displayed on screen
-//        onView(withItemText(newNoteDescription)).check(matches(isDisplayed()));
+//        fail("Implement step 7");
+        String newNoteTitle = "Espresso";
+        String newNoteDescription = "UI testing for Android185645614511";
+
+        // Click on the add note button
+        onView(withId(R.id.fab_add_notes)).perform(click());
+
+        // Add note title and description
+        // Type new note title
+        onView(withId(R.id.add_note_title)).perform(typeText(newNoteTitle), closeSoftKeyboard());
+        onView(withId(R.id.add_note_description)).perform(typeText(newNoteDescription),
+                closeSoftKeyboard()); // Type new note description and close the keyboard
+
+        // Save the note
+        onView(withId(R.id.fab_add_notes)).perform(click());
+
+        //This test is flaky. Simple check is to tell Espresso to sleep
+        Thread.sleep(5000);
+
+        // Scroll notes list to added note, by finding its description
+        onView(withId(R.id.notes_list)).perform(
+                scrollTo(hasDescendant(withText(newNoteDescription))));
+
+        // Verify note is displayed on screen
+        onView(withItemText(newNoteDescription)).check(matches(isDisplayed()));
     }
 
 }
